a80c9c88121e0d4aec359da7db5d644057fd875f
KimYuNa <hugyaa@gmail.com>
2023-03-29 22:14:09 +0900

    feat: add nav search input

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
 import './Nav.css'
 
 function Nav() {
   const [show, setShow] = useState(false)
+  const [searchValue, setSearchValue] = useState('')
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -16,6 +19,12 @@ function Nav() {
       window.removeEventListener("scroll", () => {})
     }
   }, [])
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setSearchValue(e.target.value)
+    navigate(`/search?q=${e.target.value}`)
+  }
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
@@ -23,6 +32,14 @@ function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         onClick={() => window.location.reload()}
+      />
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요"
       />
       <img
         alt="User logged"
---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useLocation } from 'react-router'
+
+export default function SearchPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
