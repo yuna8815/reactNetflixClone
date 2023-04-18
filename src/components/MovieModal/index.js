1d78d2e7a97a09505dc84ad7c755204afafe37a1
KimYuNa <hugyaa@gmail.com>
2023-04-18 22:28:29 +0900

    feat: useRef, custom hooks add useOnClickOutside

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import useOnClickOutside from '../../hooks/useOnClickOutside'
 import './MovieModal.css'
 
 export default function MovieModal({
@@ -11,10 +12,14 @@ export default function MovieModal({
   vote_average,
   setModalOpen
 }) {
+  const ref = useRef()
+  useOnClickOutside(ref, () => {
+    setModalOpen(false)
+  })
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
           <span
             onClick={() => setModalOpen(false)}
             className="modal-close"
